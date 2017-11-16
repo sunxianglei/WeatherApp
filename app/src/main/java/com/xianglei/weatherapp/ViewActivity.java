b8447b4467e1f343655bf94aa3c5e0bfa6345f90
sunxianglei <381671896@qq.com>
2017-11-17 00:29:53 +0800

    commit

--- app/src/main/java/com/xianglei/weatherapp/ViewActivity.java
+++ app/src/main/java/com/xianglei/weatherapp/ViewActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.xianglei.weatherapp.view.PeriodProgress;
+
 /**
  * Created by sheng on 2017/11/16.
  */
@@ -14,9 +16,13 @@ public class ViewActivity extends AppCompatActivity {
     private String[] time = {"10-16", "10-17", "2017-10-20", "10-21"};
     private Boolean[] isFinish = {true, false ,false, false};
 
+    private PeriodProgress mPeriodProgress;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+        mPeriodProgress = findViewById(R.id.progress);
+        mPeriodProgress.setData(name, time, isFinish);
     }
 }
--- app/src/main/java/com/xianglei/weatherapp/view/PeriodProgress.java
+++ app/src/main/java/com/xianglei/weatherapp/view/PeriodProgress.java
@@ -1,6 +1,7 @@
 package com.xianglei.weatherapp.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -9,6 +10,7 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.xianglei.weatherapp.R;
 import com.xianglei.weatherapp.utils.DisplayUtil;
 
 /**
@@ -21,10 +23,16 @@ public class PeriodProgress extends View {
     private String[] name;
     private String[] time;
     private Boolean[] isFinish;
-
     private int mWidth;
     private int mHeight;
-    private int mCircleRadius;      //圆的半径
+
+    private int mTextSize;
+    private int mTextColor;
+    private int mTextFinishColor;
+    private int mCircleRadius;
+    private int mCircleColor;
+    private int mLineColor;
+    private int mLineFinishColor;
 
     private Paint mLinePaint;
     private Paint mCirclePaint;
@@ -37,10 +45,10 @@ public class PeriodProgress extends View {
 
     public PeriodProgress(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(attrs);
     }
 
-    private void init(){
+    private void init(AttributeSet attrs){
         mLinePaint = new Paint();
         mLinePaint.setAntiAlias(true);
         mCirclePaint = new Paint();
@@ -48,6 +56,17 @@ public class PeriodProgress extends View {
         mTextPaint = new Paint();
         mTextPaint.setAntiAlias(true);
         mRect = new Rect();
+
+        TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.PeriodProgress);
+        mTextSize = ta.getDimensionPixelSize(R.styleable.PeriodProgress_progress_text_size,
+                DisplayUtil.sp2px(getContext(),13));
+        mTextColor = ta.getColor(R.styleable.PeriodProgress_progress_text_color, Color.GRAY);
+        mTextFinishColor = ta.getColor(R.styleable.PeriodProgress_progress_text_finish_color, Color.DKGRAY);
+        mCircleRadius = ta.getDimensionPixelSize(R.styleable.PeriodProgress_progress_circle_radius,
+                DisplayUtil.dip2px(getContext(),8));
+        mCircleColor = ta.getColor(R.styleable.PeriodProgress_progress_circle_color, Color.LTGRAY);
+        mLineColor = ta.getColor(R.styleable.PeriodProgress_progress_line_color, Color.LTGRAY);
+        mLineFinishColor = ta.getColor(R.styleable.PeriodProgress_progress_line_finish_color, Color.GRAY);
     }
 
     public void setData(String[] name, String[] time, Boolean[] isFinish){
@@ -66,8 +85,20 @@ public class PeriodProgress extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        mTextPaint.setTextSize(mTextSize);
+        mCirclePaint.setColor(mCircleColor);
+        mLinePaint.setStrokeWidth(DisplayUtil.dip2px(getContext(), 3));
         int length = name.length;
         for(int i=0;i<length;i++){
+            if(isFinish[i]){
+                mTextPaint.setColor(mTextFinishColor);
+                mCirclePaint.setStyle(Paint.Style.FILL);
+                mLinePaint.setColor(mLineFinishColor);
+            }else{
+                mTextPaint.setColor(mTextColor);
+                mCirclePaint.setStyle(Paint.Style.STROKE);
+                mLinePaint.setColor(mLineColor);
+            }
             mTextPaint.getTextBounds(name[i], 0, name[i].length(), mRect);
             canvas.drawText(name[i], mWidth * i / (length - 1) - (mRect.right-mRect.left)/2,
                     mTextPaint.getFontSpacing(), mTextPaint);
@@ -76,12 +107,11 @@ public class PeriodProgress extends View {
                     mTextPaint.getFontSpacing() * 2, mTextPaint);
             mCirclePaint.setColor(Color.BLACK);
             canvas.drawCircle(mWidth * i / (length - 1),
-                    mTextPaint.getFontSpacing() * 3 + mCircleRadius, mCircleRadius, mCirclePaint);
+                    mTextPaint.getFontSpacing() * 3, mCircleRadius, mCirclePaint);
 
             if(i != length - 1){
-                mLinePaint.setStrokeWidth(DisplayUtil.dip2px(getContext(), 5));
-                canvas.drawLine(mWidth * i / (length - 1) + 2 * mCircleRadius, mTextPaint.getFontSpacing() * 3 + mCircleRadius,
-                        mWidth * (i + 1) / (length - 1), mTextPaint.getFontSpacing() * 3 + mCircleRadius, mLinePaint);
+                canvas.drawLine(mWidth * i / (length - 1) + 2 * mCircleRadius, mTextPaint.getFontSpacing() * 3,
+                        mWidth * (i + 1) / (length - 1) - 2 * mCircleRadius, mTextPaint.getFontSpacing() * 3, mLinePaint);
             }
         }
     }
